37c9be67cf72cde49ddf44f6e9d278a8b7b061b5
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-02-23 19:50:58 +0200

    add preExecution

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,33 @@
-import { ethers } from "hardhat";
+import { ethers } from "hardhat"
+
+// invalid predefined start times
+// const invalidFirstStartTime = 1709799300;
+// const invalidSecondStartTime = 1712477700;
+// const invalidThirdStartTime = 1715069700;
+// const invalidFourthStartTime = 1717748100;
+
+// valid predefined start times
+// const firstStartTime = 1715069700;
+// const secondStartTime = 1723018500;
+// const thirdStartTime = 1730967300;
+// const fourthStartTime = 1738916100;
 
 async function main() {
-  // testnet LockDealNFT - 0xe42876a77108E8B3B2af53907f5e533Cba2Ce7BE
-  // mainnet LockDealNFT - 0x3d2C83bbBbfB54087d46B80585253077509c21AE
-  const lockDealNFt = "0xe42876a77108E8B3B2af53907f5e533Cba2Ce7BE"
+    // testnet LockDealNFT - 0xe42876a77108E8B3B2af53907f5e533Cba2Ce7BE
+    // mainnet LockDealNFT - 0x3d2C83bbBbfB54087d46B80585253077509c21AE
+    const lockDealNFt = "0xe42876a77108E8B3B2af53907f5e533Cba2Ce7BE"
+    const vaultId = 10
+    const invalidTime = 1709799300
+    const validTime = 1715069700
 
-  const Web3WarsFix = await ethers.getContractFactory("Web3WarsFix");
-  const web3WarsFix = await Web3WarsFix.deploy(lockDealNFt);
-  await web3WarsFix.deployed();
+    const Web3WarsFix = await ethers.getContractFactory("Web3WarsFix")
+    const web3WarsFix = await Web3WarsFix.deploy(lockDealNFt, invalidTime, validTime, vaultId)
+    await web3WarsFix.deployed()
 
-  console.log("Web3WarsFix deployed to:", web3WarsFix.address);
+    console.log("Web3WarsFix deployed to:", web3WarsFix.address)
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.error(error)
+    process.exitCode = 1
+})
---
+++ test/Firewall.ts
@@ -0,0 +1,84 @@
+import { ERC20Token, MockFirewall, MockVaultManager, Web3WarsFix } from "../typechain-types"
+import { LockDealNFT } from "../typechain-types"
+import { LockDealProvider, DealProvider } from "../typechain-types"
+import { time, mine, takeSnapshot, SnapshotRestorer } from "@nomicfoundation/hardhat-network-helpers"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { deployed } from "@poolzfinance/poolz-helper-v2"
+import { expect } from "chai"
+import { ethers } from "hardhat"
+
+describe("Web3WarsFix tests", function () {
+    let lockProvider: LockDealProvider
+    let lockDealNFT: LockDealNFT
+    let web3WarsFix: Web3WarsFix
+    let firewall: MockFirewall
+    let token: ERC20Token
+    let mockVaultManager: MockVaultManager
+    let poolId: number
+    let receiver: SignerWithAddress
+    let addresses: string[]
+    let snapShot: SnapshotRestorer
+    let params: [number, number]
+    const amount = ethers.utils.parseUnits("1", 18)
+    const MAX_RATIO = ethers.utils.parseUnits("1", 21)
+    const signature: Uint8Array = ethers.utils.toUtf8Bytes("signature")
+    const invalidFirstStartTime = 1709799300
+    const firstStartTime = 1715069700
+
+    before(async () => {
+        [receiver] = await ethers.getSigners()
+        token = (await deployed("ERC20Token", "TEMP")) as ERC20Token
+        mockVaultManager = (await deployed("MockVaultManager")) as MockVaultManager
+        lockDealNFT = (await deployed("LockDealNFT", mockVaultManager.address, "")) as LockDealNFT
+        const dealProvider = (await deployed("DealProvider", lockDealNFT.address)) as DealProvider
+        lockProvider = (await deployed(
+            "LockDealProvider",
+            lockDealNFT.address,
+            dealProvider.address
+        )) as LockDealProvider
+        await lockDealNFT.setApprovedContract(lockProvider.address, true)
+        await token.approve(mockVaultManager.address, MAX_RATIO.mul(10))
+        web3WarsFix = (await deployed(
+            "Web3WarsFix",
+            lockDealNFT.address,
+            invalidFirstStartTime.toString(),
+            firstStartTime.toString(),
+            "10"
+        )) as Web3WarsFix
+        firewall = (await deployed("MockFirewall", web3WarsFix.address)) as MockFirewall
+        await mockVaultManager.setWeb3War(true)
+        await lockProvider.setFirewall(firewall.address)
+        snapShot = await takeSnapshot()
+    })
+
+    afterEach(async () => {
+        await snapShot.restore()
+    })
+
+    async function createNewPool(invalidTime: number) {
+        poolId = (await lockDealNFT.totalSupply()).toNumber()
+        params = [amount, invalidTime]
+        addresses = [receiver.address, token.address]
+        await lockProvider.createNewPool(addresses, params, signature)
+    }
+
+    it("should revert withdraw tokens before valid time", async () => {
+        await createNewPool(invalidFirstStartTime)
+        await time.setNextBlockTimestamp(invalidFirstStartTime)
+        await expect(
+            lockDealNFT
+                .connect(receiver)
+                ["safeTransferFrom(address,address,uint256)"](receiver.address, lockDealNFT.address, poolId)
+        ).to.be.rejectedWith("Web3WarsFix: invalid time")
+    })
+
+    it("should pass withdraw tokens after valid time", async () => {
+        await createNewPool(invalidFirstStartTime)
+        await time.setNextBlockTimestamp(firstStartTime)
+        await expect(
+            lockDealNFT
+                .connect(receiver)
+                ["safeTransferFrom(address,address,uint256)"](receiver.address, lockDealNFT.address, poolId)
+        ).not.to.be.rejected
+    })
+})
