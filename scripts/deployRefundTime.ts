c1f6756296f2922ac14fdeb4c6a5b944d662a232
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-12-12 12:20:53 +0200

    update deploy script, add comment

--- scripts/deployRefundTime.ts
+++ scripts/deployRefundTime.ts
@@ -1,17 +1,11 @@
 import { ethers } from "hardhat"
 
-// invalid predefined start times
-// const invalidFirstStartTime = 1709799300;
-
-// valid predefined start times
-// const firstStartTime = 1715069700;
-
 async function main() {
-    const invalidTime = 1709799300
-    const validTime = 1715069700
+    const collateralPoolId = 64841
+    const correctTime = 1702395000
 
     const RefundTimeOverride = await ethers.getContractFactory("RefundTimeOverride")
-    const refundTimeOverride = await RefundTimeOverride.deploy(validTime, validTime)
+    const refundTimeOverride = await RefundTimeOverride.deploy(collateralPoolId, correctTime)
     await refundTimeOverride.deployed()
 
     console.log("RefundTimeOverride deployed to:", refundTimeOverride.address)
